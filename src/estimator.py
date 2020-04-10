4292e26f9c5fa7c112c81836cde43458499c669a
Emmanuel Oluwatobi <emmanueloluwatobi2000@gmail.com>
2020-04-10 05:16:00 +0100

    init

--- src/estimator.py
+++ src/estimator.py
@@ -12,8 +12,19 @@ def estimator(data):
     elif covid_data['periodType'] == 'months':
         impact_infections_by_request_time = int(impact_currently_infected*(2**(covid_data['timeToElapse']*30/3)))
         severe_impact_infections_by_request_time=int(severe_impact_curently_infected*(2**(covid_data['timeToElapse']*30/3)))
+    
+    python_return_data = {
+      "impact": {
+        "currentlyInfected": impact_currently_infected,
+        "infectionsByRequestTime": impact_infections_by_request_time,
+      },
+      "severeImpact": {
+        "currentlyInfected": severe_impact_curently_infected,
+        "infectionsByRequestTime": severe_impact_infections_by_request_time,
+      }
+    }
 
 
-    return (impact_currently_infected,severe_impact_curently_infected,impact_infections_by_request_time,severe_impact_infections_by_request_time)
+    return json.dumps(python_return_data)
 json_str = '{"region": {"name": "Africa", "avgAge": 19.7, "avgDailyIncomeInUSD": 5, "avgDailyIncomePopulation": 0.71}, "periodType": "days", "timeToElapse": 58, "reportedCases": 674, "population": 66622705, "totalHospitalBeds": 1380614 }'
 print(estimator(json_str))
